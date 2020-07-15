ae45bbb9a5a4a911a5d2a34778aa0560ada4f625
Relanium <48989277+mrz2419@users.noreply.github.com>
2020-07-15 17:34:25 +0500

    Первая загрузка

---
+++ discord_bot/config.py
@@ -0,0 +1,16 @@
+TOKEN = '********************' #bot token
+
+POST_ID = ************** #post ID to read reactions from
+
+#roles list according to emotes
+ROLES = {
+    '🐔': ******************, #Dota gamer
+    '🐒': ******************, #COD gamer
+    '🌈': ******************, #MMO RPG gamer
+    '😎': *******************, #Just talk
+    }
+
+#exclude this roles from counting
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 2 #max ammount of roles a user can have
